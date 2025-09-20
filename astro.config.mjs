80c1b2e5ecbef49209761e45495eedf401ed110b
Sam Richard <snugug@google.com>
2025-09-20 11:09:40 -0400

    Add support for figures and message blocks to Markdown

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,14 @@ import * as path from 'path';
 import * as url from 'url';
 import tsconfig from './tsconfig.json';
 import pagefind from './lib/pagefind';
+import svelte from '@astrojs/svelte';
+import mdx from '@astrojs/mdx';
+import {
+  remarkDefinitionList,
+  defListHastHandlers,
+} from 'remark-definition-list';
+import remarkDirective from 'remark-directive';
+import { remarkContainers } from './lib/markdown';
 
 // Get the current directory
 const __dirname = url.fileURLToPath(new URL('.', import.meta.url));
@@ -16,16 +24,20 @@ const aliases = Object.entries(tsconfig.compilerOptions.paths).map(
   }),
 );
 
-import svelte from '@astrojs/svelte';
-
-import mdx from '@astrojs/mdx';
-
 // https://astro.build/config
 export default defineConfig({
   integrations: [svelte(), pagefind(), mdx()],
   vite: {
     resolve: {
       alias: aliases,
+    },
+  },
+  markdown: {
+    remarkPlugins: [remarkDefinitionList, remarkDirective, remarkContainers],
+    remarkRehype: {
+      handlers: {
+        ...defListHastHandlers,
+      },
     },
   },
 });
---
+++ lib/markdown.ts
@@ -0,0 +1,75 @@
+import { h } from 'hastscript';
+import { visit } from 'unist-util-visit';
+
+// This plugin is an example to turn `::note` into divs,
+// passing arbitrary attributes.
+export function remarkContainers() {
+  /**
+   * @param {Root} tree
+   *   Tree.
+   * @returns {undefined}
+   *   Nothing.
+   */
+  return (tree) => {
+    visit(tree, (node) => {
+      if (
+        node.type === 'containerDirective' ||
+        node.type === 'leafDirective' ||
+        node.type === 'textDirective'
+      ) {
+        /* Usage
+        :::message{.warning|.info|.error}
+
+        Stuff Goes Here
+
+        :::
+        */
+        if (node.name === 'message') {
+          if (node.attributes.class) {
+            node.attributes['data-type'] = node.attributes.class;
+            node.attributes.class += ' message';
+          } else {
+            node.attributes.class = 'message';
+          }
+
+          const data = node.data || (node.data = {});
+          const tagName = node.type === 'textDirective' ? 'span' : 'div';
+
+          data.hName = tagName;
+          data.hProperties = h(tagName, node.attributes || {}).properties;
+        } else if (node.name === 'figure') {
+          /* Usage
+        :::figure[Caption Goes Here]{#id}
+
+        Stuff Goes Here
+
+        :::
+        */
+          if (node.attributes.id) {
+            node.attributes.id = `figure-${node.attributes.id}`;
+          }
+
+          // Find Figcaption
+          const label = node.children.findIndex((c) => c.data.directiveLabel);
+          if (label >= 0) {
+            const capt = node.children[label];
+            const captData = capt.data || {};
+            captData.hName = 'figcaption';
+            captData.hProperties = h('figcaption', capt || {}).properties;
+            node.children = node.children.splice(label - 1, 1);
+            node.children.push(capt);
+          }
+
+          // Build node back up
+          const data = node.data || (node.data = {});
+          const tagName = 'figure';
+
+          data.hName = tagName;
+          data.hProperties = h(tagName, node.attributes || {}).properties;
+        } else {
+          return;
+        }
+      }
+    });
+  };
+}
