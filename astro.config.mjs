e88ec58ef75f67ba910bc839629e7a6351f7ed26
Sam Richard <snugug@google.com>
2025-12-20 07:56:48 -0500

    Remove unneeded aliasing from Astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,24 +1,10 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-import * as path from 'path';
-import * as url from 'url';
-import tsconfig from './tsconfig.json';
 import pagefind from './lib/pagefind';
 import svelte from '@astrojs/svelte';
 import mdx from '@astrojs/mdx';
 import { markdown } from './lib/markdown';
 import istanbul from 'vite-plugin-istanbul';
-
-// Get the current directory
-const __dirname = url.fileURLToPath(new URL('.', import.meta.url));
-
-// Get the aliases from tsconfig
-const aliases = Object.entries(tsconfig.compilerOptions.paths).map(
-  ([key, value]) => ({
-    find: key.replace(/\/\*$/, ''),
-    replacement: path.join(__dirname, value[0]).replace(/\/\*$/, ''),
-  }),
-);
 
 // https://astro.build/config
 export default defineConfig({
@@ -33,9 +19,6 @@ export default defineConfig({
           requireEnv: false,
         }),
     ],
-    resolve: {
-      alias: aliases,
-    },
     worker: {
       format: 'es',
     },
